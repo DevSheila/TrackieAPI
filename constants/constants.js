476d37dbf94fdda6d9151e70352ea756861cb5ed
Sheila Sharon <devsheilawambui@gmail.com>
2022-09-09 15:00:54 +0300

    basic authentication and generate otp and email to user

---
+++ constants/constants.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+module.exports = {
+  allowedOrigins: ['http://localhost:5000/'],
+  SERVER_PORT: process.env.PORT || 5000,
+  SERVER_DB_URI: process.env.DB_URI,
+  JWT_SECRET: 'thisIsASimpleTest',
+  OTP_LENGTH: 4,
+  OTP_CONFIG: {
+    lowerCaseAlphabets:false,
+    upperCaseAlphabets: false,
+    specialChars: false,
+  },
+  MAIL_SETTINGS: {
+    service: 'gmail',
+    auth: {
+      type: 'OAuth2',
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      accessToken:process.env.OAUTH_ACCESS_TOKEN
+    },
+  },
+};
---
+++ server.js
@@ -0,0 +1,19 @@
+const { app, db,PORT } = require('./config/config');
+const { SERVER_DB_URI } = require('./constants/constants');
+const connectDB = async () => {
+  try {
+    await db.connect(SERVER_DB_URI,{useNewUrlParser:true,useUnifiedTopology: true},()=>{
+      console.log("Connected to DB!");
+    })
+
+    //start server
+    app.listen(PORT, async () => {
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectDB();
+
+
+
